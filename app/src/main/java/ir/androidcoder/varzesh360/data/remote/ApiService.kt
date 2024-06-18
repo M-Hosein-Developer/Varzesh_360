3728e0920b5e5268cec3aec614cea42c94e1fd4b
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-06-17 22:01:21 -1200

    Setup Remote And Debug

--- app/src/main/java/ir/androidcoder/varzesh360/data/remote/ApiService.kt
+++ app/src/main/java/ir/androidcoder/varzesh360/data/remote/ApiService.kt
@@ -1,14 +1,15 @@
 package ir.androidcoder.varzesh360.data.remote
 
+import ir.androidcoder.varzesh360.domain.model.NewsListResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("football360/news/info")
-    fun getNewsInfo(
-        @Query("id") newsId: String = "news_id",
+    @GET("football360/news/list")
+    fun getNewsList(
+        @Query("page") page: Int,
         @Query("license") license: String = "14kDIdzjZw8wqCzb9CxOrPtQpgSNmllIDSKqiq"
-    )
+    ) : NewsListResponse
 
 }
