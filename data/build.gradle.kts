08dd14c565e2164d9656b7b664fb0287c9b53967
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-06-20 23:41:56 -1200

    Create Data Module

---
+++ data/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("java-library")
+    alias(libs.plugins.jetbrains.kotlin.jvm)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies{
+
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(project(":domain"))
+
+}
