4a93830db97ba2fa8581a19056c28cf3cff2de45
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-06-23 20:34:37 -1200

    Setup News View Model

--- presentatiom/src/main/java/ir/androidcoder/varzesh360/viewModel/NewsViewModel.kt
+++ presentatiom/src/main/java/ir/androidcoder/varzesh360/viewModel/NewsViewModel.kt
@@ -1,4 +1,55 @@
 package ir.androidcoder.varzesh360.viewModel
 
-class NewsViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import ir.androidcoder.domain.usecase.NewsUseCase
+import ir.androidcoder.varzesh360.newsIntent.NewsIntent
+import ir.androidcoder.varzesh360.newsState.NewsState
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NewsViewModel @Inject constructor(private val newsUseCase: NewsUseCase) : ViewModel() {
+
+    val dataIntent = Channel<NewsIntent>()
+
+    //News State
+    private val _newsState = MutableStateFlow<NewsState>(NewsState.Idle)
+    val newsState : StateFlow<NewsState> get() = _newsState
+
+    init {
+        handleIntent()
+    }
+
+    private fun handleIntent() {
+
+        viewModelScope.launch {
+
+            dataIntent.consumeAsFlow().collect {
+
+                when (it) {
+                    is NewsIntent.FetchNewsData -> newsDate(it.pageNumber)
+                }
+
+            }
+
+        }
+
+    }
+
+    private fun newsDate(pageNumber: Int) = viewModelScope.launch {
+
+         try {
+             _newsState.value = NewsState.NewsData(newsUseCase.getNews(pageNumber))
+         }catch (e : Exception){
+             _newsState.value = NewsState.NewsError(e.localizedMessage)
+         }
+
+    }
+
 }
