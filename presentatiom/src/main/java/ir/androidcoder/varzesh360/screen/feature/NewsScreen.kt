7b148105fe2aa9759ed8b7f9a54503e0e725f294
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-06-24 08:05:22 -1200

    Create News Screen

---
+++ presentatiom/src/main/java/ir/androidcoder/varzesh360/screen/feature/NewsScreen.kt
@@ -0,0 +1,11 @@
+package ir.androidcoder.varzesh360.screen.feature
+
+import androidx.compose.runtime.Composable
+import ir.androidcoder.varzesh360.viewModel.NewsViewModel
+
+@Composable
+fun NewsScreen(newsViewModel: NewsViewModel) {
+
+
+
+}
