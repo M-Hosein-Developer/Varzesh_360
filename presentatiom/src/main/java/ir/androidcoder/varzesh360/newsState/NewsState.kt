91d8d7dda8e150c7361179dc75ceb3b725d0a843
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-06-23 19:43:59 -1200

    Setup News State

--- presentatiom/src/main/java/ir/androidcoder/varzesh360/newsState/NewsState.kt
+++ presentatiom/src/main/java/ir/androidcoder/varzesh360/newsState/NewsState.kt
@@ -1,4 +1,11 @@
 package ir.androidcoder.varzesh360.newsState
 
+import ir.androidcoder.domain.entities.NewsEntity
+
 sealed class NewsState {
+
+    data object Idle : NewsState()
+    data class NewsData(val news: List<NewsEntity>) : NewsState()
+    data class NewsError(val error : String?) : NewsState()
+
 }
