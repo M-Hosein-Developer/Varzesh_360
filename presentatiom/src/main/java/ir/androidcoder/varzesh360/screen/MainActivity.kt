41ccb8bcfd718813f07befa4cfcd25a90d59fd74
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-06-24 08:06:06 -1200

    Setup Bottom Bar

--- presentatiom/src/main/java/ir/androidcoder/varzesh360/screen/MainActivity.kt
+++ presentatiom/src/main/java/ir/androidcoder/varzesh360/screen/MainActivity.kt
@@ -1,47 +1,119 @@
 package ir.androidcoder.varzesh360.screen
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import ir.androidcoder.varzesh360.screen.feature.NewsScreen
 import ir.androidcoder.varzesh360.screen.ui.theme.Varzesh360Theme
+import ir.androidcoder.varzesh360.util.MyScreen
+import ir.androidcoder.varzesh360.viewModel.NewsViewModel
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val newsViewModel: NewsViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             Varzesh360Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+
+                    MainUi(newsViewModel)
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun GreetingPreview() {
-    Varzesh360Theme {
-        Greeting("Android")
+fun MainUi(newsViewModel: NewsViewModel) {
+
+    val navController = rememberNavController()
+    var isVisible by remember { mutableStateOf(true) }
+
+
+    if (isVisible) {
+        NavigationBar(
+            modifier = Modifier
+                .wrapContentSize(align = Alignment.BottomCenter)
+                .height(112.dp)
+                .padding(horizontal = 42.dp)
+                .padding(bottom = 32.dp, top = 12.dp)
+                .clip(RoundedCornerShape(32.dp))
+        ) {
+
+            NavigationBarItem(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(top = 24.dp),
+                selected = false,
+                onClick = { },
+                icon = {
+                    Icon(
+                        imageVector = Icons.Outlined.Home,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(text = "Home")
+                }
+            )
+
+            NavigationBarItem(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(top = 24.dp),
+                selected = false,
+                onClick = { },
+                icon = {
+                    Icon(
+                        imageVector = Icons.Outlined.Settings,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(text = "Setting")
+                }
+            )
+
+        }
     }
+
+
 }
