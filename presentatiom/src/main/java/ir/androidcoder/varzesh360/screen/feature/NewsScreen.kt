3d447063e41c1799dc7acdbd6a65d245aa76cd3a
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-06-25 08:04:24 -1200

    Setup News Toolbar

--- presentatiom/src/main/java/ir/androidcoder/varzesh360/screen/feature/NewsScreen.kt
+++ presentatiom/src/main/java/ir/androidcoder/varzesh360/screen/feature/NewsScreen.kt
@@ -1,11 +1,87 @@
 package ir.androidcoder.varzesh360.screen.feature
 
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import ir.androidcoder.varzesh360.R
 import ir.androidcoder.varzesh360.viewModel.NewsViewModel
 
 @Composable
 fun NewsScreen(newsViewModel: NewsViewModel) {
 
+    val context = LocalContext.current
 
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(horizontal = 24.dp)
+            .padding(top = 42.dp, bottom = 8.dp)
+    ) {
+
+        CostumeToolbar(
+            {  },
+            {  }
+        )
+
+
+    }
+
+}
+
+@Composable
+fun CostumeToolbar(onProfileClicked :() -> Unit , onAlarmClicked :() -> Unit){
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+
+    ) {
+
+        AsyncImage(
+            model = R.drawable.ic_launcher_background,
+            contentDescription = null,
+            modifier = Modifier
+                .clip(RoundedCornerShape(16.dp))
+                .clickable { onProfileClicked.invoke() }
+        )
+
+        Icon(
+            imageVector = Icons.Outlined.Notifications,
+            contentDescription = null,
+            modifier = Modifier
+                .size(52.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .clickable(true, "", null, { onAlarmClicked.invoke() })
+                .border(1.dp, Color.LightGray, RoundedCornerShape(16.dp))
+                .padding(8.dp)
+
+        )
+
+    }
 
 }
