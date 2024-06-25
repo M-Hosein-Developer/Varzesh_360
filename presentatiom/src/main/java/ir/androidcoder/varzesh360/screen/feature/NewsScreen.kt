359d131d5736f15bb13938e01ef6a1c5f16a1cf4
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-06-25 09:27:27 -1200

    Add Search News In Search Screen

--- presentatiom/src/main/java/ir/androidcoder/varzesh360/screen/feature/NewsScreen.kt
+++ presentatiom/src/main/java/ir/androidcoder/varzesh360/screen/feature/NewsScreen.kt
@@ -1,9 +1,9 @@
 package ir.androidcoder.varzesh360.screen.feature
 
-import android.widget.Toast
-import androidx.compose.foundation.background
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,14 +16,23 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import ir.androidcoder.varzesh360.R
 import ir.androidcoder.varzesh360.viewModel.NewsViewModel
@@ -32,6 +41,7 @@ import ir.androidcoder.varzesh360.viewModel.NewsViewModel
 fun NewsScreen(newsViewModel: NewsViewModel) {
 
     val context = LocalContext.current
+    var counter by remember { mutableStateOf(1) }
 
     Column(
         Modifier
@@ -45,6 +55,19 @@ fun NewsScreen(newsViewModel: NewsViewModel) {
             {  }
         )
 
+        SearchNews(
+            textValue = newsViewModel.searchValue,
+            counter,
+            {
+                newsViewModel.searchValue = it
+            },
+            {
+                counter++
+            },
+            {
+                counter = 0
+            }
+        )
 
     }
 
@@ -81,6 +104,57 @@ fun CostumeToolbar(onProfileClicked :() -> Unit , onAlarmClicked :() -> Unit){
                 .padding(8.dp)
 
         )
+
+    }
+
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun SearchNews(
+    textValue: String,
+    counter: Int,
+    onValueChange: (String) -> Unit,
+    onCounterClicked: () -> Unit,
+    onCounterLongClicked: () -> Unit
+) {
+
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 14.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        OutlinedTextField(
+            value = textValue,
+            onValueChange = { onValueChange.invoke(it) },
+            label = { Text(text = "جستجو") },
+            shape = RoundedCornerShape(18.dp)
+        )
+
+       Surface(
+        modifier = Modifier
+            .border(1.dp, Color.LightGray, RoundedCornerShape(18.dp))
+            .padding(horizontal = 28.dp)
+            .padding(vertical = 18.dp)
+            .combinedClickable(
+            onClick = { onCounterClicked.invoke() },
+            onLongClick = { onCounterLongClicked.invoke() }
+        )
+    ) {
+
+        Text(
+            text = counter.toString(),
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp
+            )
+        )
+
+    }
 
     }
 
