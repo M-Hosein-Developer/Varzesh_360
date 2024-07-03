fdaf2c18002043398905e44181c9a3fa8298fa26
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-07-03 06:15:11 -1200

    Nullable Model Class

--- data/src/main/java/ir/androidcoder/data/local/entities/NewsListEntity.kt
+++ data/src/main/java/ir/androidcoder/data/local/entities/NewsListEntity.kt
@@ -5,19 +5,19 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class NewsListEntity(
-    val code: Int,
-    val created_at: Int,
+    val code: Int = 0,
+    val created_at: Int = 0,
     @PrimaryKey
-    val id: String,
-    val infoAPI: String,
-    val link: String,
-    val live_detail: Any,
-    val post_type: String,
-    val published_at: Int,
-    val slug: String,
-    val sub_title: String,
-    val super_title: Any,
-    val title: String
+    val id: String = "",
+    val infoAPI: String = "",
+    val link: String = "",
+    val live_detail: Any? = null,
+    val post_type: String = "",
+    val published_at: Int = 0,
+    val slug: String = "",
+    val sub_title: String = "",
+    val super_title: Any? = null,
+    val title: String = ""
 )
 
 
--- data/src/main/java/ir/androidcoder/data/model/NewsListResponse.kt
+++ data/src/main/java/ir/androidcoder/data/model/NewsListResponse.kt
@@ -1,41 +1,41 @@
 package ir.androidcoder.data.model
 
 data class NewsListResponse(
-    val `by`: String,
-    val channel: String,
-    val documents: String,
-    val github: String,
-    val npm: String,
-    val result: List<Result>,
-    val website: String
+    val `by`: String = "",
+    val channel: String = "",
+    val documents: String = "",
+    val github: String = "",
+    val npm: String = "",
+    val result: List<Result> = emptyList(),
+    val website: String = ""
 ) {
     data class Result(
-        val code: Int,
-        val created_at: Int,
-        val id: String,
-        val infoAPI: String,
-        val link: String,
-        val live_detail: Any,
-        val post_type: String,
-        val primary_media: PrimaryMedia,
-        val published_at: Int,
-        val slug: String,
-        val sub_title: String,
-        val super_title: Any,
-        val title: String
+        val code: Int = 0,
+        val created_at: Int = 0,
+        val id: String = "",
+        val infoAPI: String = "",
+        val link: String = "",
+        val live_detail: Any? = null,
+        val post_type: String = "",
+        val primary_media: PrimaryMedia = PrimaryMedia(),
+        val published_at: Int = 0,
+        val slug: String = "",
+        val sub_title: String = "",
+        val super_title: Any? = null,
+        val title: String = ""
     ) {
         data class PrimaryMedia(
-            val cover_image: String,
-            val duration: Int,
-            val `file`: String,
-            val hour_duration: Int,
-            val id: String,
-            val media_type: String,
-            val minute_duration: Int,
-            val second_duration: Int,
-            val thumbnail: String,
-            val title: String,
-            val upload_video_link: String
+            val cover_image: String = "",
+            val duration: Int = 0,
+            val `file`: String = "",
+            val hour_duration: Int = 0,
+            val id: String = "",
+            val media_type: String = "",
+            val minute_duration: Int = 0,
+            val second_duration: Int = 0,
+            val thumbnail: String = "",
+            val title: String = "",
+            val upload_video_link: String = ""
         )
     }
 }
