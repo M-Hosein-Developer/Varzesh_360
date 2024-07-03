c38ee8543cf3f29264f13963798e8f2f844b05c3
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-07-03 07:19:08 -1200

    Setup News Dao

--- data/src/main/java/ir/androidcoder/data/local/NewsDao.kt
+++ data/src/main/java/ir/androidcoder/data/local/NewsDao.kt
@@ -1,10 +1,20 @@
 package ir.androidcoder.data.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ir.androidcoder.data.local.entities.NewsListEntity
 
 
 @Dao
 interface NewsDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNewsData(data: List<NewsListEntity>)
+
+
+    @Query("SELECT * FROM NewsListEntity WHERE id = :id")
+    suspend fun selectDetailNewsData(id : String) : NewsListEntity
 
 }
