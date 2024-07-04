9da82948ea6964229f4134a2d2afb4fb06a1d667
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-07-04 07:57:41 -1200

    Setup News Data Db Intent

--- presentatiom/src/main/java/ir/androidcoder/varzesh360/newsIntent/NewsIntent.kt
+++ presentatiom/src/main/java/ir/androidcoder/varzesh360/newsIntent/NewsIntent.kt
@@ -3,5 +3,6 @@ package ir.androidcoder.varzesh360.newsIntent
 sealed class NewsIntent {
 
     data class FetchNewsData(val pageNumber : Int) : NewsIntent()
+    data class FetchNewsDataFromDb(val id : String) : NewsIntent()
 
 }
