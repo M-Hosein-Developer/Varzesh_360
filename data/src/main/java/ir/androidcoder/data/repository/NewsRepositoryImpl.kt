f55c55f086aecee28813dbe142f092f6e13bfa8f
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-07-06 22:26:44 -1200

    Setup News Repository

--- data/src/main/java/ir/androidcoder/data/repository/NewsRepositoryImpl.kt
+++ data/src/main/java/ir/androidcoder/data/repository/NewsRepositoryImpl.kt
@@ -2,13 +2,11 @@ package ir.androidcoder.data.repository
 
 import android.util.Log
 import ir.androidcoder.data.local.NewsDao
-import ir.androidcoder.data.mapper.fromResponse
+import ir.androidcoder.data.mapper.mapResponseToEntity
 import ir.androidcoder.data.mapper.toDomainEntity
-import ir.androidcoder.data.mapper.toNewsEntity
 import ir.androidcoder.data.remote.ApiService
 import ir.androidcoder.domain.entities.NewsEntity
 import ir.androidcoder.domain.repository.NewsRepository
-import ir.androidcoder.util.primaryMedia
 
 class NewsRepositoryImpl(private val apiService: ApiService, private val dao: NewsDao) : NewsRepository {
 
@@ -17,7 +15,7 @@ class NewsRepositoryImpl(private val apiService: ApiService, private val dao: Ne
         val data = apiService.getNewsList(pageNumber, "14kDIdzjZw8wqCzb9CxOrPtQpgSNmllIDSKqiq")
 
         try {
-            dao.insertNewsData(fromResponse(data))
+            dao.insertNewsData(mapResponseToEntity(data))
             Log.v("getNews", "Data inserted into database")
         } catch (e: Exception) {
             Log.e("getNews", "Error inserting data into database: ${e.message}", e)
@@ -28,7 +26,6 @@ class NewsRepositoryImpl(private val apiService: ApiService, private val dao: Ne
 
 
     override suspend fun getNewsDetailFromDb(id: String): NewsEntity = dao
-        .selectDetailNewsData(id)
-        .toNewsEntity(primaryMedia)
+        .selectDetailNewsData(id).toDomainEntity()
 
 }
