fa0bacf1c03a562e531991c7a8217310e7f30869
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-07-06 22:23:59 -1200

    ReDesign News Entity Db Class

--- data/src/main/java/ir/androidcoder/data/local/entities/NewsListEntity.kt
+++ data/src/main/java/ir/androidcoder/data/local/entities/NewsListEntity.kt
@@ -13,10 +13,25 @@ data class NewsListEntity(
     val link: String = "",
     val live_detail: String? = "",
     val post_type: String = "",
+    val primary_media: PrimaryMedia = PrimaryMedia(),
     val published_at: Int = 0,
     val slug: String = "",
-    val sub_title: String? = "" ,
+    val sub_title: String? = "",
     val super_title: String? = "",
     val title: String = ""
-)
+){
+    data class PrimaryMedia(
+        val cover_image: String? = "",
+        val duration: Int? = 0,
+        val `file`: String? = "",
+        val hour_duration: Int? = 0,
+        val id: String? = "",
+        val media_type: String? = "",
+        val minute_duration: Int? = 0,
+        val second_duration: Int? = 0,
+        val thumbnail: String? = "",
+        val title: String? = "",
+        val upload_video_link: String? = ""
+    )
+}
 
