90e885ae598f7a34dcd823c0e57d7e7dd23f5e60
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-07-06 22:35:29 -1200

    Get Data From Db And Create

--- presentatiom/src/main/java/ir/androidcoder/varzesh360/screen/feature/DetailScreen.kt
+++ presentatiom/src/main/java/ir/androidcoder/varzesh360/screen/feature/DetailScreen.kt
@@ -1,12 +1,17 @@
 package ir.androidcoder.varzesh360.screen.feature
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
 import ir.androidcoder.domain.entities.NewsEntity
 import ir.androidcoder.varzesh360.newsIntent.NewsIntent
 import ir.androidcoder.varzesh360.newsState.NewsDetailState
@@ -37,4 +42,30 @@ fun DetailScreen(navController: NavHostController, id: String, newsViewModel: Ne
     }
 
 
+    Column(
+        Modifier
+            .fillMaxSize()
+    ) {
+
+        if (data != null){
+
+            FullScreenImage(data!!)
+
+        }
+
+    }
+
 }
+
+@Composable
+fun FullScreenImage(data: NewsEntity) {
+
+    AsyncImage(
+        model =  data.primary_media.file,
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.fillMaxSize()
+    )
+
+}
+
