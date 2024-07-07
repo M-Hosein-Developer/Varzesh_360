c9661668a5ae8256fc92a5eab0697b3ac7cdfc75
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-07-06 22:25:59 -1200

    New Setup Mapper

--- data/src/main/java/ir/androidcoder/data/mapper/NewDataToDomainData.kt
+++ data/src/main/java/ir/androidcoder/data/mapper/NewDataToDomainData.kt
@@ -3,20 +3,32 @@ package ir.androidcoder.data.mapper
 import ir.androidcoder.data.local.entities.NewsListEntity
 import ir.androidcoder.domain.entities.NewsEntity
 
-fun NewsListEntity.toNewsEntity(primaryMedia: NewsEntity.PrimaryMedia): NewsEntity {
-    return NewsEntity(
-        code = this.code,
-        created_at = this.created_at,
-        id = this.id,
-        infoAPI = this.infoAPI,
-        link = this.link,
-        live_detail = this.live_detail ?: "",
-        post_type = this.post_type,
-        primary_media = primaryMedia,
-        published_at = this.published_at,
-        slug = this.slug,
-        sub_title = this.sub_title ?: "",
-        super_title = this.super_title ?: "",
-        title = this.title
-    )
-}
+fun NewsListEntity.toDomainEntity() = NewsEntity(
+    code = code,
+    created_at = created_at,
+    id = id,
+    infoAPI = infoAPI ?: "",
+    link = link ?: "",
+    live_detail = live_detail ?: "",
+    post_type = post_type ?: "",
+    primary_media = primary_media.toDomainEntity(),
+    published_at = published_at,
+    slug = slug ?: "",
+    sub_title = sub_title ?: "",
+    super_title = super_title ?: "",
+    title = title ?: ""
+)
+
+fun NewsListEntity.PrimaryMedia.toDomainEntity() = NewsEntity.PrimaryMedia(
+    cover_image = cover_image ?: "",
+    duration = duration ?: 0,
+    file = file ?: "",
+    hour_duration = hour_duration ?: 0,
+    id = id ?: "",
+    media_type = media_type ?: "",
+    minute_duration = minute_duration ?: 0,
+    second_duration = second_duration ?: 0,
+    thumbnail = thumbnail ?: "",
+    title = title ?: "",
+    upload_video_link = upload_video_link ?: ""
+)
--- data/src/main/java/ir/androidcoder/data/mapper/NewsDatabaseMapper.kt
+++ data/src/main/java/ir/androidcoder/data/mapper/NewsDatabaseMapper.kt
@@ -3,7 +3,7 @@ package ir.androidcoder.data.mapper
 import ir.androidcoder.data.local.entities.NewsListEntity
 import ir.androidcoder.data.model.NewsListResponse
 
-fun fromResponse(response: NewsListResponse): List<NewsListEntity> {
+fun mapResponseToEntity(response: NewsListResponse): List<NewsListEntity> {
     return response.result.map { result ->
         NewsListEntity(
             code = result.code,
@@ -11,13 +11,30 @@ fun fromResponse(response: NewsListResponse): List<NewsListEntity> {
             id = result.id,
             infoAPI = result.infoAPI,
             link = result.link,
-//            live_detail = result.live_detail,
+            live_detail = result.live_detail,
             post_type = result.post_type,
+            primary_media = mapPrimaryMedia(result.primary_media),
             published_at = result.published_at,
             slug = result.slug,
-            sub_title = result.sub_title ?: "",
-//            super_title = result.super_title,
+            sub_title = result.sub_title,
+            super_title = result.super_title,
             title = result.title
         )
     }
+}
+
+private fun mapPrimaryMedia(primaryMedia: NewsListResponse.Result.PrimaryMedia): NewsListEntity.PrimaryMedia {
+    return NewsListEntity.PrimaryMedia(
+        cover_image = primaryMedia.cover_image,
+        duration = primaryMedia.duration,
+        file = primaryMedia.`file`,
+        hour_duration = primaryMedia.hour_duration,
+        id = primaryMedia.id,
+        media_type = primaryMedia.media_type,
+        minute_duration = primaryMedia.minute_duration,
+        second_duration = primaryMedia.second_duration,
+        thumbnail = primaryMedia.thumbnail,
+        title = primaryMedia.title,
+        upload_video_link = primaryMedia.upload_video_link
+    )
 }
