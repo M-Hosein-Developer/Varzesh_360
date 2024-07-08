cd400fc7907bd45eae151fe171ecfac9407a3b3d
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-07-08 08:01:37 -1200

    Add Toolbar In Detail Screen

--- presentatiom/src/main/java/ir/androidcoder/varzesh360/screen/feature/DetailScreen.kt
+++ presentatiom/src/main/java/ir/androidcoder/varzesh360/screen/feature/DetailScreen.kt
@@ -2,6 +2,13 @@ package ir.androidcoder.varzesh360.screen.feature
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -49,11 +56,30 @@ fun DetailScreen(navController: NavHostController, id: String, newsViewModel: Ne
 
         if (data != null){
 
+            DetailToolbar{
+                navController.popBackStack()
+            }
+
             FullScreenImage(data!!)
 
         }
 
     }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailToolbar(onBackPressed: () -> Unit) {
+
+    TopAppBar(
+        title = { Text(text = "Detail") },
+        navigationIcon = {
+            IconButton(onClick = { onBackPressed.invoke() }) {
+                Icon(imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft, contentDescription = null)
+            }
+        }
+    )
 
 }
 
