acba0a2cdb832c2602c5418352f0364ce395c5a5
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-07-08 09:09:02 -1200

    Setup Detail Title

--- presentatiom/src/main/java/ir/androidcoder/varzesh360/screen/feature/DetailScreen.kt
+++ presentatiom/src/main/java/ir/androidcoder/varzesh360/screen/feature/DetailScreen.kt
@@ -1,5 +1,6 @@
 package ir.androidcoder.varzesh360.screen.feature
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,6 +10,7 @@ import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -19,14 +21,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush.Companion.linearGradient
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import ir.androidcoder.domain.entities.NewsEntity
 import ir.androidcoder.varzesh360.newsIntent.NewsIntent
 import ir.androidcoder.varzesh360.newsState.NewsDetailState
+import ir.androidcoder.varzesh360.screen.ui.theme.NoColor
 import ir.androidcoder.varzesh360.viewModel.NewsViewModel
 
 @Composable
@@ -102,7 +109,27 @@ fun FullScreenImage(data: NewsEntity) {
             modifier = Modifier.fillMaxSize()
         )
 
-
+        Text(
+            text = data.title,
+            modifier = Modifier.align(
+                Alignment.BottomCenter
+            )
+                .background(
+                    brush = linearGradient(
+                        colors = listOf(NoColor, MaterialTheme.colorScheme.onPrimary),
+                        start = androidx.compose.ui.geometry.Offset(500f, 0f),
+                        end = androidx.compose.ui.geometry.Offset(500f, 850f)
+                    )
+                )
+                .padding(horizontal = 28.dp)
+                .padding(bottom = 72.dp , top = 150.dp),
+            textAlign = TextAlign.End,
+            style = TextStyle(
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = MaterialTheme.typography.bodyLarge.fontFamily
+            )
+        )
     }
 
 }
