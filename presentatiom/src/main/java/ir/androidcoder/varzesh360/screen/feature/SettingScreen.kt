10d60d477579b589d1eb2f2fa05d1c232c2d1252
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-07-09 22:46:29 -1200

    Create Dynamic Switch Ui

--- presentatiom/src/main/java/ir/androidcoder/varzesh360/screen/feature/SettingScreen.kt
+++ presentatiom/src/main/java/ir/androidcoder/varzesh360/screen/feature/SettingScreen.kt
@@ -1,24 +1,47 @@
 package ir.androidcoder.varzesh360.screen.feature
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SettingScreen(){
 
+    var dynamic by remember { mutableStateOf(false) }
 
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
     ) {
 
         SettingToolbar{
 
+        }
+
+        ThemeSetting(dynamic){
+            dynamic = it
         }
 
     }
@@ -44,4 +67,55 @@ fun SettingToolbar(onResetClicked :() -> Unit){
             }
         },
     )
+}
+
+@Composable
+fun ThemeSetting(
+    dynamicThemeState : Boolean ,
+    onDynamicThemeState :(Boolean) -> Unit
+){
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 12.dp),
+    ) {
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.End
+        ){
+
+            Text(
+                text = "تم داینامیک",
+                Modifier.padding(end = 12.dp)
+            )
+
+            Switch(
+                checked = dynamicThemeState,
+                onCheckedChange = {
+                    onDynamicThemeState.invoke(it)
+                },
+                thumbContent = if (dynamicThemeState) {
+                    {
+                        Icon(
+                            imageVector = Icons.Filled.Check,
+                            contentDescription = null,
+                            modifier = Modifier.size(SwitchDefaults.IconSize),
+                        )
+                    }
+                } else {
+                    null
+                }
+            )
+
+        }
+
+    }
+
+
+
+
+
 }
