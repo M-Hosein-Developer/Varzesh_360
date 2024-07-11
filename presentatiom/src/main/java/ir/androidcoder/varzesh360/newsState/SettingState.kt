83129c46ac33c1546fb2f73353652152568ef974
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-07-10 22:09:10 -1200

    Setup Setting State

--- presentatiom/src/main/java/ir/androidcoder/varzesh360/newsState/SettingState.kt
+++ presentatiom/src/main/java/ir/androidcoder/varzesh360/newsState/SettingState.kt
@@ -1,6 +1,11 @@
 package ir.androidcoder.varzesh360.newsState
 
+import ir.androidcoder.domain.entities.SettingEntity
+
 sealed class SettingState {
 
+    data object Idle : SettingState()
+    data class SettingData(val setting : SettingEntity) : SettingState()
+    data class SettingError(val error : String?) : SettingState()
 
 }
