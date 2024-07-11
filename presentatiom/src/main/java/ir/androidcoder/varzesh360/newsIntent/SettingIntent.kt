4c1b81341a23678a36c85b8462c4be6180539c0d
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-07-10 22:06:44 -1200

    Setup Setting Intent

--- presentatiom/src/main/java/ir/androidcoder/varzesh360/newsIntent/SettingIntent.kt
+++ presentatiom/src/main/java/ir/androidcoder/varzesh360/newsIntent/SettingIntent.kt
@@ -1,7 +1,9 @@
 package ir.androidcoder.varzesh360.newsIntent
 
+import ir.androidcoder.domain.entities.SettingEntity
+
 sealed class SettingIntent {
 
-
+    data class FetchSettingData(val setting : SettingEntity) : SettingIntent()
 
 }
