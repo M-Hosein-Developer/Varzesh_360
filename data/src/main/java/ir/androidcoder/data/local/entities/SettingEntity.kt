b6b670319520b82b8c69c0d5bde4ccbb98840077
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-07-19 06:11:39 -1200

    Debugging Setting Entity

--- data/src/main/java/ir/androidcoder/data/local/entities/SettingEntity.kt
+++ data/src/main/java/ir/androidcoder/data/local/entities/SettingEntity.kt
@@ -7,6 +7,7 @@ import androidx.room.PrimaryKey
 data class SettingEntity(
 
     @PrimaryKey
+    val id : Int = 0,
     val dynamicTheme : Boolean,
     val darkTheme : Boolean,
 
--- domain/src/main/java/ir/androidcoder/domain/entities/SettingEntity.kt
+++ domain/src/main/java/ir/androidcoder/domain/entities/SettingEntity.kt
@@ -2,6 +2,7 @@ package ir.androidcoder.domain.entities
 
 data class SettingEntity(
 
+    val id : Int,
     val dynamicTheme : Boolean,
     val darkTheme : Boolean,
 
